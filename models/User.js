7d851c72a9386b31d46e5cc0f5e4ea241a74815e
sithija-ws <sithija.ws@gmail.com>
2026-09-24 22:25:56 +0530

    feat: create user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    userId : {
+        type : String,
+        required: true,
+        unique: true
+    },
+    name : {
+        type : String,
+        required: true,
+        trim: true
+    },
+    email : {
+        type : String,
+        required: true,
+        unique: true,
+        lowercase : true,
+        trim : true
+    },
+    password : {
+        type : String,
+        required: true
+    },
+    role : {
+        type : String,
+        enum : ["student", "admin", "tutor"],
+        required: true
+    },
+    faculty : {
+        type : String,
+        required : true
+    },
+    status : {
+        type : String,
+        required : true
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
