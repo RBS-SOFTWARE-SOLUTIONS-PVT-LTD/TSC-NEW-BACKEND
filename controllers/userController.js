4bac5e7b21a00ea731ad28c5e223145b36a277a7
sithija-ws <sithija.ws@gmail.com>
2026-09-25 22:27:39 +0530

    feat : create login fuction for students and tutors

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 export const SignUp = async (req, res)=>{
     try {
@@ -65,6 +66,81 @@ export const getAllUsers = async (req, res)=>{
         res.status(500).json({
             success : false,
             message: "Server error during Fetch users 👎"
+        });
+    }
+}
+
+export const Login = async (req, res)=>{
+    try {
+        const {email, password} = req.body;
+
+        //check if both given
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "Email and password 're required!"
+            });
+        }
+
+        //find user
+        const user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message : "Invalid email or password!"
+            });
+        }
+
+        //check pswd
+        const isPasswordCorrect = await bcrypt.compare(
+            password, user.password
+        );
+
+        if(!isPasswordCorrect){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password!"
+            });
+        }
+
+        if(user.status == "suspended"){
+            return res.status(403).json({
+                success: false,
+                message: "User account has been suspended!"
+            });
+        }
+
+        //create jwt
+        const token = await jwt.sign(
+            {
+                userId : user._id,
+                role : user.role
+            },
+            "thawakalika password eka",
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        //send respons
+        res.status(200).json({
+            success: true,
+            message : "User login in successfully!",
+            user : {
+                id : user._id,
+                name : user.name,
+                email : user.email,
+                role : user.role,
+                faculty : user.faculty,
+            },
+            token : token
+        })
+    } catch (error) {
+        console.log("Error: "+ error.nessage);
+        res.status(500).json({
+            success: false,
+            message : "Internal Sever Error!"
         });
     }
 }
